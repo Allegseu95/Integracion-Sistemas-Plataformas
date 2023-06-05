035b67045af4b377b9a4218c76dbd39dc4ea1008
Alex Santacruz <alexdavid_95_@hotmail.com>
2023-06-05 17:56:50 -0500

    feat(practica): :sparkles: add sockect frontend

---
+++ Parcial 2/practica-02/socket-frontend/src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        border: '2px solid red',
+      }}
+    >
+      <h2>Alex</h2>
+    </div>
+  );
+}
+
+export default App;
